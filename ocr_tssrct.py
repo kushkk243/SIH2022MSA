a8de7f2cd31878acc24dcd79be4a4ae73ea18395
KushKumarK <91371411+KushKumarK@users.noreply.github.com>
2022-03-07 13:45:10 +0530

    OCR Test 1

--- ocr_tssrct.py
+++ ocr_tssrct.py
@@ -1,58 +1,17 @@
-import json
-import os
-import sys
-import requests
-import time
-# If you are using a Jupyter Notebook, uncomment the following line.
-# %matplotlib inline
-import matplotlib.pyplot as plt
-from matplotlib.patches import Polygon
-from PIL import Image
-from io import BytesIO
+import cv2
+from easyocr import Reader
 
-missing_env = False
-# Add your Computer Vision subscription key and endpoint to your environment variables.
-endpoint = "https://sih2022msa.cognitiveservices.azure.com"
-subscription_key = "30cb1b653c124b95bf433136e275e1a3"
+lang = ['en', 'hi', '']
 
-text_recognition_url = endpoint + "/vision/v3.1/read/analyze"
 
-# Set image_url to the URL of an image that you want to recognize.
-image_url = "https://raw.githubusercontent.com/MicrosoftDocs/azure-docs/master/articles/cognitive-services/Computer-vision/Images/readsample.jpg"
+def read_txt(file_path):
+    image = cv2.imread(file_path)
+    reader = Reader(lang, gpu=True)
+    output = reader.readtext(image)
+    output_tt = []
+    for tupls in output:
+        output_tt.append(tupls[1])
+    return output_tt
 
-headers = {'Ocp-Apim-Subscription-Key': subscription_key,'Content-Type': 'application/octet-stream'}
-with open(r'..\SIH2022MSA\images\invoice-sample.jpg', 'rb') as f:
-    data = f.read()
-response = requests.post(
-    text_recognition_url, headers=headers, data=data)
-response.raise_for_status()
 
-# Extracting text requires two API calls: One call to submit the
-# image for processing, the other to retrieve the text found in the image.
 
-# Holds the URI used to retrieve the recognized text.
-operation_url = response.headers["Operation-Location"]
-
-# The recognized text isn't immediately available, so poll to wait for completion.
-analysis = {}
-poll = True
-while (poll):
-    response_final = requests.get(
-        response.headers["Operation-Location"], headers=headers)
-    analysis = response_final.json()
-
-    print(json.dumps(analysis, indent=4))
-
-    time.sleep(1)
-    if ("analyzeResult" in analysis):
-        poll = False
-    if ("status" in analysis and analysis['status'] == 'failed'):
-        poll = False
-
-polygons = []
-if ("analyzeResult" in analysis):
-    # Extract the recognized text, with bounding boxes.
-    polygons = [(line["text"])
-                for line in analysis["analyzeResult"]["readResults"][0]["lines"]]
-print(" ".join(polygons))
-# Display the image and overlay it with the extracted text.
