d2cb6cb4243582c560ec8eacff1cbc8aa2a184d1
KushKumarK <91371411+KushKumarK@users.noreply.github.com>
2022-03-09 10:18:00 +0530

    Ankit Files updated

--- blur.py
+++ blur.py
@@ -1,19 +1,19 @@
 import cv2
-
-# Read the image
-img = cv2.imread('F:\\Blur Detection\\0.jpg')
+def detect_blur(file_path):
+    #Read the image
+    img = cv2.imread(file_path)
 
 # Convert to greyscale
-grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # Find the laplacian of this image and
 # calculate the variance
-var = cv2.Laplacian(grey, cv2.CV_64F).var()
+    var = cv2.Laplacian(grey, cv2.CV_64F).var()
 
 # if variance is less than the set threshold
 # image is blurred otherwise not
-if var < 60 :
-    print('Image is Blurred')
-else:
-    print('Image Not Blurred')
+    if var < 60 :
+        return False
+    else:
+        return True
 
